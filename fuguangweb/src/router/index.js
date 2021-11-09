d2e96b9f1981a4851d7ea01a754e7061fa2ec350
QiaoPengjun <qpj99992021@163.com>
2021-11-09 00:41:16 -0800

    客户端按需加载element-plus前端组件

--- fuguangweb/src/router/index.js
+++ fuguangweb/src/router/index.js
@@ -2,6 +2,24 @@ import {createRouter, createWebHistory} from "vue-router";
 
 // 路由列表
 const routes = [
+    {
+        meta: {
+            title: "浮光在线教育-首页",
+            keepalive: true
+        },
+        path: '/',
+        name: "Home",
+        component: ()=> import("../views/Home.vue")
+    },
+    {
+        meta: {
+            title: "浮光在线教育-用户登录",
+            keepalive: true
+        },
+        path: '/login',
+        name: "Login",
+        component: ()=> import("../views/Login.vue")
+    }
 
 ]
 
---
+++ fuguangweb/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>首页</h1>
+    <el-row>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button>中文</el-button>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+import { Search, Edit, Check, Message, Star, Delete } from '@element-plus/icons'
+
+</script>
+
+<style scoped>
+
+</style>
