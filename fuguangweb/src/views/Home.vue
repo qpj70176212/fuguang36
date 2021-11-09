e38e17a1dd5f2cd3eafcdde6084f09e9563b048b
QiaoPengjun <qpj99992021@163.com>
2021-11-09 01:00:13 -0800

    客户端显示首页

--- fuguangweb/src/views/Home.vue
+++ fuguangweb/src/views/Home.vue
@@ -1,19 +1,14 @@
 <template>
-  <h1>首页</h1>
-    <el-row>
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-    <el-button>中文</el-button>
-  </el-row>
+  <div class="home">
+    <Header></Header>
+
+    <Footer></Footer>
+  </div>
 </template>
 
-<script setup lang="ts">
-import { Search, Edit, Check, Message, Star, Delete } from '@element-plus/icons'
-
+<script setup>
+import Header from "../components/Header.vue"
+import Footer from "../components/Footer.vue"
 </script>
 
 <style scoped>
