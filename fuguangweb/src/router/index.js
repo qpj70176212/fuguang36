369135ae2e143cc2284a075010462c51421f5521
QiaoPengjun <qpj99992021@163.com>
2021-11-09 00:10:21 -0800

    前端初始化并安装集成vue-router

---
+++ fuguangweb/src/router/index.js
@@ -0,0 +1,14 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+// 路由列表
+const routes = [
+
+]
+
+// 路由对象实例化
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
