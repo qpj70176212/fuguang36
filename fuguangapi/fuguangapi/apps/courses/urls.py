f406c9654fa02c2020b1f9fa06d601876283bb5a
QiaoPengjun <qpj99992021@163.com>
2021-11-18 03:40:32 -0800

    后端实现学习方向列表接口

--- fuguangapi/fuguangapi/apps/courses/urls.py
+++ fuguangapi/fuguangapi/apps/courses/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-
+    path("directions/", views.CourseDirectionListAPIView.as_view()),
 ]
--- fuguangapi/fuguangapi/apps/courses/views.py
+++ fuguangapi/fuguangapi/apps/courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
+from .models import CourseDirection
+from .serializers import CourseDirectionModelSerializer
 # Create your views here.
+
+
+class CourseDirectionListAPIView(ListAPIView):
+    """学习方向"""
+    queryset = CourseDirection.objects.filter(is_show=True, is_delete=False).order_by("orders", "-id")
+    serializer_class = CourseDirectionModelSerializer
